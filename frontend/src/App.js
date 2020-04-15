146da655db311f22cf669df28d2fedfd74778cf5
Eduardo Ferronato <edu.fe.guin@gmail.com>
2020-04-15 13:43:40 -0300

    1Logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+  }
+  return (
+    <div>
+      <Header>Contador: {counter}</Header>;
+      <button onClick={increment}>Imcrementar</button>
+    </div>
+  );
 }
 
 export default App;
